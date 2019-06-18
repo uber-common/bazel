160c4ef6d7c88217019fc145c1ceb172923e12e5
Alessandro Patti <apatti@uber.com>
2019-06-18 17:53:37 +0200

    Disable include validation in cpp rules

--- src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
@@ -93,7 +93,7 @@ public class BazelCppSemantics implements AspectLegalCppSemantics {
 
   @Override
   public boolean needsIncludeValidation() {
-    return true;
+    return false;
   }
 
   @Override
