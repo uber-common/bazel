a793459d198f87f957da6c82d35df539566e2785
Chirag Ramani <chirag.ramani@uber.com>
2021-09-22 19:53:03 +0530

    db8baa390f: Add json formatter for cquery

--- src/main/java/com/google/devtools/build/lib/query2/cquery/ConfiguredTargetQueryEnvironment.java
+++ src/main/java/com/google/devtools/build/lib/query2/cquery/ConfiguredTargetQueryEnvironment.java
@@ -249,6 +249,8 @@ public class ConfiguredTargetQueryEnvironment
             OutputType.JSON),
         new BuildOutputFormatterCallback(
             eventHandler, cqueryOptions, out, skyframeExecutor, accessor),
+        new JsonOutputFormatterCallback(
+            eventHandler, cqueryOptions, out, skyframeExecutor, accessor));
         new GraphOutputFormatterCallback(
             eventHandler,
             cqueryOptions,
