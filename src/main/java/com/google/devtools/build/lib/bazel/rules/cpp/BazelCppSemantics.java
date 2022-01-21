35ddb8e41d588d21800bd687c1d0a87418413f16
Chirag Ramani <chirag.ramani@uber.com>
2022-01-21 14:29:03 +0530

    Refer 9f24c3065f74566eb011b3342e3dddf183e87e14

--- src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/cpp/BazelCppSemantics.java
@@ -128,7 +128,7 @@ public class BazelCppSemantics implements AspectLegalCppSemantics {
 
   @Override
   public boolean needsIncludeValidation() {
-    return language != Language.OBJC;
+    return false;
   }
 
   @Override
