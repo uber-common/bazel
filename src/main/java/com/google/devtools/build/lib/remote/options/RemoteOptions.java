5702c0826c4a3b815313fc90a0db40f63f740576
Chirag Ramani <chirag.ramani@uber.com>
2022-01-21 14:40:24 +0530

    Indented

--- src/main/java/com/google/devtools/build/lib/remote/options/RemoteOptions.java
+++ src/main/java/com/google/devtools/build/lib/remote/options/RemoteOptions.java
@@ -584,12 +584,12 @@ public final class RemoteOptions extends CommonRemoteOptions {
   public long remoteMerkleTreeCacheSize;
 
   @Option(
-          name = "remote_compress_cas_uploads",
-          defaultValue =  "false",
-          documentationCategory = OptionDocumentationCategory.REMOTE,
-          effectTags = {OptionEffectTag.EXECUTION},
-          help = "If set to true, Bazel will compress CAS blobs before upload and "
-                  + "decompress them on download if using an HTTP cache.")
+      name = "remote_compress_cas_uploads",
+      defaultValue =  "false",
+      documentationCategory = OptionDocumentationCategory.REMOTE,
+      effectTags = {OptionEffectTag.EXECUTION},
+      help = "If set to true, Bazel will compress CAS blobs before upload and "
+        + "decompress them on download if using an HTTP cache.")
   public boolean remoteCompressCasUploads;
 
   @Option(
