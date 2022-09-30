c8beeea98139fb4d627f3e6c8587f5965231af63
Oli <oliviern@uber.com>
2022-09-30 14:14:56 -0700

    Refactoring key compuation for usage tracking

--- src/main/java/com/google/devtools/build/lib/actions/ActionCacheChecker.java
+++ src/main/java/com/google/devtools/build/lib/actions/ActionCacheChecker.java
@@ -256,7 +256,9 @@ public class ActionCacheChecker {
     }
 
     // Output debug info about action if needed
-    actionInputUsageTracker.dump(action);
+    if (actionInputUsageTracker.supportsInputTracking(action)) {
+      System.out.println(actionInputUsageTracker.dump(action));
+    }
 
     return !Arrays.equals(MetadataDigestUtils.fromMetadata(mdMap), entry.getFileDigest());
   }
