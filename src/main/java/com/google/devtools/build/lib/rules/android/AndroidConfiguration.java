4eb42b1f621636be6f0d522ac634e12b507690ba
Oli <oliviern@uber.com>
2023-08-01 12:21:00 +0200

    Add experimental configuration to have local host and android platform be shared

--- src/main/java/com/google/devtools/build/lib/rules/android/AndroidConfiguration.java
+++ src/main/java/com/google/devtools/build/lib/rules/android/AndroidConfiguration.java
@@ -1120,6 +1120,14 @@ public class AndroidConfiguration extends Fragment implements AndroidConfigurati
                 + " transition` with changed options to avoid potential action conflicts.")
     public boolean androidPlatformsTransitionsUpdateAffected;
 
+    @Option(
+            name = "experimental_disable_android_platform_distinguisher",
+            defaultValue = "false",
+            documentationCategory = OptionDocumentationCategory.UNDOCUMENTED,
+            effectTags = {OptionEffectTag.AFFECTS_OUTPUTS},
+            help = "If set to true, the android output path will share the same location as platform host.")
+    public boolean experimentalDisableAndroidPlatformDistinguisher;
+
     @Override
     public FragmentOptions getHost() {
       Options host = (Options) super.getHost();
@@ -1150,6 +1158,7 @@ public class AndroidConfiguration extends Fragment implements AndroidConfigurati
       host.persistentBusyboxTools = persistentBusyboxTools;
       host.disableNativeAndroidRules = disableNativeAndroidRules;
       host.persistentMultiplexBusyboxTools = persistentMultiplexBusyboxTools;
+      host.experimentalDisableAndroidPlatformDistinguisher = experimentalDisableAndroidPlatformDistinguisher;
 
       // Unless the build was started from an Android device, host means MAIN.
       host.configurationDistinguisher = ConfigurationDistinguisher.MAIN;
@@ -1211,6 +1220,8 @@ public class AndroidConfiguration extends Fragment implements AndroidConfigurati
   private final boolean hwasan;
   private final boolean getJavaResourcesFromOptimizedJar;
   private final boolean includeProguardLocationReferences;
+
+  private final boolean experimentalDisableAndroidPlatformDistinguisher;
 
   public AndroidConfiguration(BuildOptions buildOptions) throws InvalidConfigurationException {
     Options options = buildOptions.get(Options.class);
@@ -1277,6 +1288,7 @@ public class AndroidConfiguration extends Fragment implements AndroidConfigurati
     this.hwasan = options.hwasan;
     this.getJavaResourcesFromOptimizedJar = options.getJavaResourcesFromOptimizedJar;
     this.includeProguardLocationReferences = options.includeProguardLocationReferences;
+    this.experimentalDisableAndroidPlatformDistinguisher = options.experimentalDisableAndroidPlatformDistinguisher;
 
     if (incrementalDexingShardsAfterProguard < 0) {
       throw new InvalidConfigurationException(
@@ -1536,7 +1548,7 @@ public class AndroidConfiguration extends Fragment implements AndroidConfigurati
 
   @Override
   public String getOutputDirectoryName() {
-    return configurationDistinguisher.suffix;
+    return experimentalDisableAndroidPlatformDistinguisher ? "" : configurationDistinguisher.suffix;
   }
 
   public boolean filterRJarsFromAndroidTest() {
