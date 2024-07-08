ed0900df84d4279c76ca69e79c429da7ecf58072
Olivier Notteghem <oliviern@uber.com>
2024-07-08 16:22:40 +0200

    Enable omitting external input only in cache key scrubber
    Summary: In order to obtain reproducible builds and consistent remote cache key between build, add the ability to only scrub files that are external to package. For example, *_resources.jar and *.jdeps are only used by module *internal* actions and we don't necessarily need these inputs to be used by dependant targets because these are unfortunatly not 100% deterministic and, luckily, they are in fact unused. This is relevant as these would cause remote cache key not to be consistent, and often leading to remote cache miss.
    
    Test Plan:
    ```
    USE_LOCAL_BAZEL=1 ./bazelw purge
    USE_LOCAL_BAZEL=1 ./bazelw clean --expunge
    USE_LOCAL_BAZEL=1 ./bazelw build eatsDebug --remote_cache=grpcs://buildfarm-androidxx.uberinternal.com
    cp build/report.json build/report1.json
    USE_LOCAL_BAZEL=1 ./bazelw purge
    USE_LOCAL_BAZEL=1 ./bazelw clean --expunge
    USE_LOCAL_BAZEL=1 ./bazelw build eatsDebug --remote_cache=grpcs://buildfarm-androidxx.uberinternal.com
    cp build/report.json build/report2.json
    /Users/oliviern/Uber/android/tooling/bazel/report_verify.py build/report1.json build/report2.json
    ```
    
    Reviewers: idanakav
    
    Revert Plan: git revert
    
    JIRA Issues: MOBDROID-6424
    
    Differential Revision: https://code.uberinternal.com/D14320679

--- src/main/java/com/google/devtools/build/lib/remote/RemoteExecutionService.java
+++ src/main/java/com/google/devtools/build/lib/remote/RemoteExecutionService.java
@@ -384,7 +384,8 @@ public class RemoteExecutionService {
                       walker,
                       inputMetadataProvider,
                       context.getPathResolver(),
-                      spawnScrubber)));
+                      spawnScrubber,
+                      spawn)));
       if (!outputDirMap.isEmpty()) {
         subMerkleTrees.add(
             MerkleTree.build(
@@ -393,6 +394,7 @@ public class RemoteExecutionService {
                 execRoot,
                 context.getPathResolver(),
                 /* spawnScrubber= */ null,
+                /* spawn= */ null,
                 digestUtil));
       }
       return MerkleTree.merge(subMerkleTrees, digestUtil);
@@ -415,6 +417,7 @@ public class RemoteExecutionService {
           execRoot,
           context.getPathResolver(),
           spawnScrubber,
+          spawn,
           digestUtil);
     }
   }
@@ -424,7 +427,8 @@ public class RemoteExecutionService {
       InputWalker walker,
       InputMetadataProvider inputMetadataProvider,
       ArtifactPathResolver artifactPathResolver,
-      @Nullable SpawnScrubber spawnScrubber)
+      @Nullable SpawnScrubber spawnScrubber,
+      Spawn spawn)
       throws IOException, ForbiddenActionInputException {
     // Deduplicate concurrent computations for the same node. It's not possible to use
     // MerkleTreeCache#get(key, loader) because the loading computation may cause other nodes to be
@@ -437,7 +441,7 @@ public class RemoteExecutionService {
       try {
         freshFuture.complete(
             uncachedBuildMerkleTreeVisitor(
-                walker, inputMetadataProvider, artifactPathResolver, spawnScrubber));
+                walker, inputMetadataProvider, artifactPathResolver, spawnScrubber, spawn));
       } catch (Exception e) {
         freshFuture.completeExceptionally(e);
       }
@@ -462,7 +466,8 @@ public class RemoteExecutionService {
       InputWalker walker,
       InputMetadataProvider inputMetadataProvider,
       ArtifactPathResolver artifactPathResolver,
-      @Nullable SpawnScrubber scrubber)
+      @Nullable SpawnScrubber scrubber,
+      Spawn spawn)
       throws IOException, ForbiddenActionInputException {
     ConcurrentLinkedQueue<MerkleTree> subMerkleTrees = new ConcurrentLinkedQueue<>();
     subMerkleTrees.add(
@@ -472,12 +477,13 @@ public class RemoteExecutionService {
             execRoot,
             artifactPathResolver,
             scrubber,
+            spawn,
             digestUtil));
     walker.visitNonLeaves(
         (Object subNodeKey, InputWalker subWalker) ->
             subMerkleTrees.add(
                 buildMerkleTreeVisitor(
-                    subNodeKey, subWalker, inputMetadataProvider, artifactPathResolver, scrubber)));
+                    subNodeKey, subWalker, inputMetadataProvider, artifactPathResolver, scrubber, spawn)));
     return MerkleTree.merge(subMerkleTrees, digestUtil);
   }
 
