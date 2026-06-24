b8e5ed13928a75b1416e5f8c503f8720b5b4b4d5
cjk <cjk@uber.com>
2026-06-24 16:37:07 +0000

    Add null check for label in cache key scrubber
    Prior to this commit, there was an access to getLabel() with no null
    check. --combined_report=lcov creates an action with no label, causing
    a crash when the cache key scrubbing tries to determine whether to
    scrub the action.
    
    In this commit we add a null check to avoid the crash.

--- src/main/java/com/google/devtools/build/lib/remote/Scrubber.java
+++ src/main/java/com/google/devtools/build/lib/remote/Scrubber.java
@@ -137,7 +137,8 @@ public class Scrubber {
     private boolean matches(Spawn spawn) {
       String mnemonic = spawn.getMnemonic();
       ActionOwner actionOwner = spawn.getResourceOwner().getOwner();
-      String label = actionOwner.getLabel().getCanonicalForm();
+      String label =
+          actionOwner.getLabel() != null ? actionOwner.getLabel().getCanonicalForm() : "";
       String kind = actionOwner.getTargetKind();
       boolean isForTool = actionOwner.isBuildConfigurationForTool();
 
